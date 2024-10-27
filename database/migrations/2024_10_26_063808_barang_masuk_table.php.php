b7b612e58d6a9ff6cdae4d8292af43da0b8f9d45
Evvin_ <kevinivander930@gmail.com>
2024-10-27 21:04:25 +0700

    Initial commit

---
+++ database/migrations/2024_10_26_063808_barang_masuk_table.php.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void{
+    Schema::create('barang_masuk', function (Blueprint $table) {
+        $table->id('id_barang');
+        $table->string('nama_barang');
+        $table->date('tgl_masuk');
+        $table->integer('jml_masuk');
+        $table->unsignedBigInteger('id_supplier');
+        $table->foreign('id_supplier')->references('id')->on('supplier')->onDelete('cascade');
+        $table->string('lokasi');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang_masuk');
+    }
+};
